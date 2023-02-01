72fb19e89a91e589bff81ad8978838305bf2f7fa
Moussa <manuscrit974@gmail.com>
2023-02-01 19:35:10 +0100

    en cours

---
+++ script.js
@@ -0,0 +1,9 @@
+let uppercase_letters = "AZERTYUIOPQSDFGHJKLMWXCVBN";
+let lowercase_letters = uppercase_letters
+  .toLowerCase()
+  .split("")
+  .reverse()
+  .join("");
+let numbers = "1234567890";
+let symbols = "'&é(§è!çà)-,;:=$%";
+let new_password = [];
