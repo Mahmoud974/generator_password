cc0bf7687a940222659b9334e4429dcfbea33380
Moussa <manuscrit974@gmail.com>
2023-02-12 16:56:42 +0100

    almost finished

--- script.js
+++ script.js
@@ -1,9 +1,62 @@
-let uppercase_letters = "AZERTYUIOPQSDFGHJKLMWXCVBN";
-let lowercase_letters = uppercase_letters
-  .toLowerCase()
-  .split("")
-  .reverse()
-  .join("");
-let numbers = "1234567890";
-let symbols = "'&é(§è!çà)-,;:=$%";
-let new_password = [];
+var _a, _b;
+var uppercase_letter = "AZERTYUIOPQSDFGHJKLMWXCVBN", lowercase_letter = uppercase_letter
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .join(""), numbers_password = "1234567890";
+var symbols_password = "'&é(§è!çà)-,;:=$%";
+var new_password = [];
+var new_password_reverse;
+var new_password_one;
+var generate_form = document.getElementById('generate_form');
+var quantity = document.getElementById('quantity');
+var password = document.getElementById('password');
+var checked;
+var copy_password = document.querySelector('#copy_password');
+var nbreRange;
+(_a = document.querySelector('#different_choice')) === null || _a === void 0 ? void 0 : _a.addEventListener("change", function (e) {
+    switch (e.target.id) {
+        case "uppercase_letter":
+            new_password.push(uppercase_letter);
+            break;
+        case "lowercase_letter":
+            new_password.push(lowercase_letter);
+            break;
+        case "numbers":
+            new_password.push(numbers_password);
+            break;
+        case "symbols":
+            new_password.push(symbols_password);
+            break;
+    }
+});
+/**
+ * Barre pour la boucle for
+ */
+(_b = document.querySelector('#range_quantity')) === null || _b === void 0 ? void 0 : _b.addEventListener("change", function (e) {
+    new_password_reverse = new_password.join("").split("");
+    nbreRange = Number(e.target.value);
+    quantity.innerHTML = e.target.value;
+});
+generate_form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (new_password.length === 0 || nbreRange === undefined) {
+        alert('Veuillez remplir le generator convenable!!');
+        window.location.reload();
+    }
+    else if (nbreRange && !new_password_one) {
+        for (var index = 0; index < nbreRange; index++) {
+            new_password_one =
+                new_password_reverse[Math.round(new_password_reverse.length * Math.random())];
+            if (new_password_one === undefined) {
+                alert('Veuiller commencer par la case');
+                window.location.reload();
+                break;
+            }
+            else {
+                password.innerHTML += new_password_one;
+                password.classList.add("text-slate-100");
+            }
+        }
+    }
+});
---
+++ script.ts
@@ -0,0 +1,73 @@
+let uppercase_letter:string = "AZERTYUIOPQSDFGHJKLMWXCVBN",
+lowercase_letter:string = uppercase_letter
+  .toLowerCase()
+  .split("")
+  .reverse()
+  .join(""),numbers_password:string = "1234567890";
+let symbols_password:string = "'&é(§è!çà)-,;:=$%";
+let new_password:string[] = [];
+
+let new_password_reverse:any
+let new_password_one:string;
+let generate_form = <HTMLElement>document.getElementById('generate_form');
+let quantity =<HTMLElement>document.getElementById('quantity');
+let password=<HTMLHeadElement>document.getElementById('password');
+let checked:any;
+let copy_password = <HTMLElement>document.querySelector('#copy_password')
+
+ let nbreRange:number; 
+
+
+document.querySelector('#different_choice')?.addEventListener("change", (e:any)=> {
+
+  switch (e.target.id) {
+    case "uppercase_letter":
+      new_password.push(uppercase_letter);
+      break;
+    case "lowercase_letter":
+      new_password.push(lowercase_letter);
+      break;
+    case "numbers":
+      new_password.push(numbers_password);
+      break;
+    case "symbols":
+      new_password.push(symbols_password);
+      break;
+}
+
+
+});
+/**
+ * Barre pour la boucle for
+ */
+  document.querySelector('#range_quantity')?.addEventListener("change", (e:any):void => {
+    new_password_reverse = new_password.join("").split("");
+ nbreRange = Number(e.target.value);
+ quantity.innerHTML = e.target.value;
+  });
+
+generate_form.addEventListener("submit", (e:any):void => {
+  e.preventDefault();
+  if(new_password.length === 0 || nbreRange === undefined ){
+    
+    alert('Veuillez remplir le generator convenable!!')
+   window.location.reload()
+
+  }
+else if (nbreRange && !new_password_one  ){
+for (let index = 0; index <nbreRange; index++) {
+   new_password_one =
+    new_password_reverse[
+      Math.round(new_password_reverse.length * Math.random())
+    ];
+  if(new_password_one === undefined){
+    alert('Veuiller commencer par la case');
+       window.location.reload()
+    break;
+  }else{
+
+      password.innerHTML += new_password_one;
+       password.classList.add("text-slate-100");
+
+  }}}}
+);
